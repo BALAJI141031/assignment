b994b8609dfb4606314eb44687fd4e00e717b7d5
BALAJI141031 <85923874+BALAJI141031@users.noreply.github.com>
2022-06-07 09:29:30 +0530

    feat:key events added

--- src/App.js
+++ src/App.js
@@ -6,13 +6,47 @@ import FloatingBlock from './components/DisplaySection/floatingBlock';
 import { useState } from "react"
 import { Routes, Route, Link } from 'react-router-dom'
 import Pagetwo from './Routes/routeTwo'
+import { useEffect } from 'react';
 
 const positionTypes=["center","lower-right"]
 function App() {
   const [pageOneChanges, setpageOneChanges] = useState(false)
   const [position, setPosition] = useState(null)
+  const [floatingBlockStatus,setFloatingBlock]=useState(false)
   
-  console.log(position,"what is final positin",position)
+
+
+  // handling escape
+  useEffect(() => {
+    const handleEsc = (event) => {
+       if (event.keyCode === 27) {
+         setFloatingBlock(true)
+          // console.log("handling escape`")
+      }
+    };
+    window.addEventListener('keydown', handleEsc);
+
+    return () => {
+      window.removeEventListener('keydown', handleEsc);
+    };
+  }, []);
+
+  // handle enter
+  useEffect(() => {
+  const handleEnter = (event) => {
+       if (event.keyCode === 13) {
+         setFloatingBlock(false)
+        //  console.log("handling enter")
+      }
+    };
+    window.addEventListener('keydown', handleEnter);
+
+    return () => {
+      window.removeEventListener('keydown', handleEnter);
+    };
+  }, []);
+  
+  console.log("should render after pressing ker",floatingBlockStatus)
   return (
     <div className="App">     
       <Routes>
@@ -25,11 +59,11 @@ function App() {
                   {positionTypes.map((type) => <PositionType type={type} key={type} setPosition={setPosition} pageOneChanges={pageOneChanges} setpageOneChanges={setpageOneChanges} position={position}/>)}
             </div>
            </div>
-            <p>Welcome to the vertica test A</p>
+          <p>Welcome to the vertica test A</p>
          <Clock/>
        </header>
       <main className="display-area"  >
-              <div id={position !== null ? position : "ideal-position"}><FloatingBlock position={position} pageOneChanges={pageOneChanges}/></div>
+              <div id={position !== null ? position : "ideal-position"} style={floatingBlockStatus?{display:"none"}:{display:"block"}}><FloatingBlock position={position} pageOneChanges={pageOneChanges}/></div>
         </main> 
         <footer className='pos-fixed flex-div flex-H-space-center flex-H-center-V'>
        <button> <Link to="/page2">
@@ -37,7 +71,6 @@ function App() {
        </Link> </button>
         </footer></>} />
         
-
         <Route path="/page2" element={<Pagetwo setpageOneChanges={setpageOneChanges} setPosition={setPosition}/>}/>
       </Routes>
       
